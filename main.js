b65c26c5ed0689426f7e656927a1598a25136bbc
matthew-elgart <matthewjelgart@gmail.com>
2025-03-08 12:12:39 -0500

    pushing code that was left uncommitted when I came back here

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const { Client, Intents } = require("discord.js");
 const { token } = require("./config.json");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const ytdl = require("ytdl-core");
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require("@discordjs/voice");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
 
 client.once("ready", () => {
 	console.log("MusicBot is online!");
@@ -12,10 +14,13 @@ TODOS:
 * make a clear messages command
 * pull token from gitignored file
 * linter
-- get mvp of sounds working
+- get mvp of sounds working (start and stop)
+- queueing
+- linter autofix instead of complaining (esp for whitespace)
 - investigate buttons (try to make command bar for pause/play, skip, etc)
 - convert commands to registered slash commands
 - separate out commands into their own files
+- typescript?
 */
 
 client.on("messageCreate", async message => {
@@ -27,6 +32,39 @@ client.on("messageCreate", async message => {
 		const allMessages = await message.channel.messages.fetch({ limit: 100 });
 		console.log(`Clearing ${allMessages.size} messages.`);
 		message.channel.bulkDelete(allMessages);
+	}
+	else if (message.content === "!play" && !message.author.bot) {
+		const voiceChannel = message.member.voice.channel;
+
+		if (!voiceChannel) {
+			message.channel.send("You need to be in a voice channel to execute this command.");
+			return;
+		}
+
+		/*
+		* create a connection
+		- create an audio player
+		- subscribe the connection to the audio player (terminology?)
+		- create an audio resource from youtube
+		- audioPlayer.play(resource)
+		- encoding and all that?
+		*/
+
+		const connection = joinVoiceChannel({
+			channelId: voiceChannel.id,
+			guildId: voiceChannel.guild.id,
+			adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+		});
+		const player = createAudioPlayer();
+		// connection.subscribe(player);
+
+		const process = ytdl("https://www.youtube.com/watch?v=dQw4w9WgXcQ", { filter: "audioonly" }, { stdio: ["ignore", "pipe", "ignore"] });
+		const resource = createAudioResource(process);
+		player.play(resource);
+
+		connection.subscribe(player);
+
+		setTimeout(() => { connection.destroy(); }, 50000);
 	}
 });
 
